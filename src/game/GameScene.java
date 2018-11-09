ef8aec66ab5c604faab8239be9767c68696cd0c0
Ondra Szkandera <szkanderao@gmail.com>
2018-11-09 12:23:03 +0100

    Commit before devide scenes 2

--- src/game/GameScene.java
+++ src/game/GameScene.java
@@ -1,13 +1,58 @@
 package game;
 
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class GameScene extends Scene{
 
-	public GameScene(Parent root, double width, double height) {
+	private GameWindow window; 
+	private Canvas canvas;
+	private GraphicsContext context;
+	
+	public GameScene(Parent root, double width, double height, GameWindow window) {
 		super(root, width, height);
-		// TODO Auto-generated constructor stub
+		
+		/*//root.getChildren().add(canvas);
+		
+		this.window = window;
+		
+		//this.canvas = new Canvas(this.window.getDefaultSizeX(), this.window.getDefaultSizeY());
+		this.context = canvas.getGraphicsContext2D();
+
+
+		
+		this.setOnKeyPressed(new EventHandler<KeyEvent>(){
+			public void handle(KeyEvent e){
+				if(e.getCode() == KeyCode.RIGHT) {
+					window.setRightKeyPressed(true);
+				}
+				if(e.getCode() == KeyCode.LEFT) {
+					window.setLeftKeyPressed(true);
+				}
+			}
+		});
+	 
+	    this.setOnKeyReleased(new EventHandler<KeyEvent>(){
+	    	public void handle(KeyEvent e){
+	    		if(e.getCode() == KeyCode.RIGHT) {
+	    				window.setRightKeyPressed(false);
+	    		}
+	    		if(e.getCode() == KeyCode.LEFT) {
+	    				window.setLeftKeyPressed(false);
+	    		}
+	    	}
+	    });*/
 	}
 
+	public GraphicsContext getContext() {
+		return context;
+	}
+	
+	
 }
